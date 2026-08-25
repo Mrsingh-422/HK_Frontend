972687a7cad7dbbc527e3e28301845374cb3754a
mudabirkowsar <mudabirkowsar@gmail.com>
2026-08-25 16:50:42 +0530

    Your commit message

--- app/(user)/components/loginComponents/loginAsDoctor/LoginAsDoctor.jsx
+++ app/(user)/components/loginComponents/loginAsDoctor/LoginAsDoctor.jsx
@@ -149,7 +149,7 @@ function LoginAsDoctor() {
                 closeModal();
                 openModal("register");
               }}
-            >
+            > 
               Register?
             </span>
           </p>
